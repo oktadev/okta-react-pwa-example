32fd926d8ddd4fb019d7caf783c59a2957c181a2
Layo Folaranmi <thedarkkid.codes@gmail.com>
2025-05-05 15:04:33 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security } from '@okta/okta-react';
+import Home from './Home';
+
+const oktaAuth = new OktaAuth({
+    issuer: 'https://{yourOktaDomain}/oauth2/default',
+    clientId: '{yourClientID}',
+    redirectUri: window.location.origin + '/login/callback',
+    scopes: ['openid', 'profile', 'email', 'offline_access']
+});
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
