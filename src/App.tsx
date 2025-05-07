49cc8e73363894038df9fae662d2e06d659a1e2f
Layo Folaranmi <thedarkkid.codes@gmail.com>
2025-05-07 20:11:23 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,33 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import { LoginCallback, Security } from '@okta/okta-react';
 import Home from './Home';
+import Profile from './Profile';
 
 const oktaAuth = new OktaAuth({
-    issuer: 'https://{yourOktaDomain}/oauth2/default',
-    clientId: '{yourClientID}',
+    clientId: import.meta.env.VITE_OKTA_DOMAIN,
+    issuer: `https://${import.meta.env.VITE_OKTA_DOMAIN}/oauth2/default`,
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email', 'offline_access']
-});
+} as any) ;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const history = useHistory();
+    const restoreOriginalUri = (_oktaAuth,  originalUri) => {
+        history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/login/callback" component={LoginCallback}/>
+                    <Route path="/profile" component={Profile}/>
+                </Switch>
+            </Security>
+        </>
+    );
 }
 
 export default App
---
+++ src/Profile.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { useOktaAuth } from "@okta/okta-react";
+
+const Profile= () => {
+	const { authState, oktaAuth} = useOktaAuth();
+	const [userInfo, setUserInfo] = useState(null);
+
+	useEffect(() => {
+		if(!authState || !authState.isAuthenticated) setUserInfo(null);
+		else setUserInfo(authState.idToken.claims);
+	}, [authState, oktaAuth]);
+
+	return (userInfo) ? (
+		<div>
+			<div>
+				<h1>My User Profile (ID Token Claims)</h1>
+				<p>
+					Below is the information from your ID token which was obtained during the &nbsp;
+					<a href="https://developer.okta.com/docs/guides/implement-auth-code-pkce">PKCE Flow</a>
+					{' '}
+					and is now stored in local storage.
+				</p>
+				<p>
+					This route is protected with the
+					{' '}
+					<code>&lt;SecureRoute&gt;</code>
+					{' '}
+					component, which will ensure that this page cannot be accessed until you have
+					authenticated.
+				</p>
+				<table>
+					<thead>
+					<tr>
+						<th>Claim</th>
+						<th>Value</th>
+					</tr>
+					</thead>
+					<tbody>
+					{Object.entries(userInfo).map((claimEntry) => {
+						const claimName = claimEntry[0];
+						const claimValue = claimEntry[1];
+						const claimId = `claim-${claimName}`;
+						return (
+							<tr key={claimName}>
+								<td>{claimName}</td>
+								<td id={claimId}>{claimValue.toString()}</td>
+							</tr>
+						);
+					})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	) : (<div>
+			<p>Fetching user profile...</p>
+		</div>)
+};
+
+export default Profile;
